e36f14def6c9159513956aa200547fbba34f73a6
Aniket Dixit <64648557+anixtm3@users.noreply.github.com>
2026-02-20 03:32:59 +0530

    Update code.sh

--- modes/code.sh
+++ modes/code.sh
@@ -1,1 +1,32 @@
- 
+#!/bin/bash
+
+echo "Starting Code Mode..."
+
+pkill -f acadence_watchdog 2>/dev/null
+
+echo "💻 CODE MODE" > /tmp/acadence_mode
+
+gsettings set org.gnome.desktop.notifications show-banners false
+
+pkill firefox
+pkill discord
+pkill telegram-desktop
+pkill spotify
+
+brave --profile-directory="Profile 2" &
+code &
+gnome-terminal &
+
+bash -c '
+acadence_watchdog() {
+    while true
+    do
+        pkill firefox
+        pkill discord
+        pkill telegram-desktop
+        pkill spotify
+        sleep 3
+    done
+}
+acadence_watchdog
+' &
