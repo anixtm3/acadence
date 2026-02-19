3ea4be6d1315816b3397ef9177d45a9a943c3ce5
Aniket Dixit <64648557+anixtm3@users.noreply.github.com>
2026-02-20 03:33:12 +0530

    Update study.sh

--- modes/study.sh
+++ modes/study.sh
@@ -1,1 +1,32 @@
- 
+#!/bin/bash
+
+echo "Starting Study Mode..."
+
+pkill -f acadence_watchdog 2>/dev/null
+
+echo "📚 STUDY MODE" > /tmp/acadence_mode
+
+gsettings set org.gnome.desktop.notifications show-banners false
+
+pkill firefox
+pkill discord
+pkill telegram-desktop
+pkill spotify
+
+brave --profile-directory="Profile 1" &
+obsidian &
+evince &
+
+bash -c '
+acadence_watchdog() {
+    while true
+    do
+        pkill firefox
+        pkill discord
+        pkill telegram-desktop
+        pkill spotify
+        sleep 3
+    done
+}
+acadence_watchdog
+' &
