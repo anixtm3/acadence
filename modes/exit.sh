d05c7046752ae7d43f2123bbd9f7c50154b1c044
Aniket Dixit <64648557+anixtm3@users.noreply.github.com>
2026-02-25 12:43:43 +0530

    Update exit.sh

--- modes/exit.sh
+++ modes/exit.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# Kill watchdog first
+EXIT_PASSWORD="hakunamatata"
+
+# Ask password using popup
+input_password=$(zenity --password --title="Acadence Lock" --text="Enter password to exit Acadence Mode")
+
+# If user pressed Cancel
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+# Check password
+if [[ "$input_password" != "$EXIT_PASSWORD" ]]; then
+    zenity --error --title="Acadence" --text="❌ Wrong Password. Exit Denied."
+    exit 1
+fi
+
+# Kill watchdog
 pkill -f acadence_watchdog 2>/dev/null
 sleep 0.1
 
-# Remove top bar indicator
+# Remove mode indicator
 rm -f /tmp/acadence_mode
 
 # Re-enable notifications
 gsettings set org.gnome.desktop.notifications show-banners true
 sleep 0.2
 
-# Send exit notification
-notify-send "Acadence" "🔓 Acadence Mode Exited"
+# Success message
+zenity --info --title="Acadence" --text="🔓 Acadence Mode Exited"
